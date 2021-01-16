03d2ae673bdb52dc3c90e42c42c562b530dad3f8
thak1411 <thak1411@gmail.com>
2021-01-17 00:02:29 +0900

    Update: AtCoder KEYENCE Programming Contest 2021

--- src/AC_KPC_2021/A.cc
+++ src/AC_KPC_2021/A.cc
@@ -6,7 +6,7 @@
 #include <string>
 #include <cmath>
 typedef long long ll;
-ll a[200001], b[200001], c[200001], da[200001], db[200001];
+ll a[200001], b[200001], c[200001], da[200001];
 int main() {
     int n;
     scanf("%d", &n);
@@ -14,15 +14,10 @@ int main() {
     for (int i = 0; i < n; ++i) scanf("%d", b + i);
     c[0] = a[0] * b[0];
     da[0] = a[0];
-    db[0] = b[0];
     printf("%lld\n", c[0]);
     for (int i = 1; i < n; ++i) {
         da[i] = std::max(da[i - 1], a[i]);
-        c[i] = std::max(da[i] * db[i], a[i] * db[i]);
-        c[i] = std::max(c[i], da[i] * b[i]);
-        c[i] = std::max(c[i], c[i - 1]);
-        c[i] = std::max(c[i], a[i] * b[i]);
-        db[i] = std::max(db[i - 1], b[i]);
+        c[i] = std::max(c[i - 1], da[i] * b[i]);
         printf("%lld\n", c[i]);
     }
     return 0;
